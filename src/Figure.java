fc98afe75126075eab2597739ace4439906d6861
Алексей Баландин <w1green1w@gmail.com>
2022-05-10 23:06:16 +0500

    Initial commit

---
+++ src/Figure.java
@@ -0,0 +1,25 @@
+abstract public class Figure {
+
+    float x;
+    float y;
+
+    public void Figure(float x_, float y_){
+
+        this.x = x_;
+        this.y = y_;
+
+    }
+
+    public float getArea(){
+
+        return 0;
+
+    }
+
+    public  float getPerimeter(){
+
+        return 0;
+
+    }
+
+}
